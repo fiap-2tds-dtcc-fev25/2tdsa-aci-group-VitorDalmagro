6be8b9e3783ae04741f94899a7a84dfc9a198362
VitorDalmagro <vitordalmagro77@gmail.com>
2026-08-20 09:24:11 -0300

    ADD: script para criar Container Group

---
+++ cofre.sh
@@ -0,0 +1,18 @@
+az provider register --namespace Microsoft.KeyVault az keyvault create \ --resource-group rg-money-hub \
+  --name kv-moneyhub-rm566052 \
+  --location eastus \
+  --sku standard \
+  --enable-rbac-authorization true \
+  --output table
+
+
+az role assignment create \
+  --assignee $(az account show --query user.name -o tsv) \
+  --role "Key Vault Administrator" \
+  --scope /subscriptions/$(az account show --query id -o tsv)/resourceGroups/rg-money-hub/providers/Microsoft.KeyVault/vaults/kv-moneyhub-rm566052
+
+
+# az keyvault secret set \
+#  --vault-name kv-moneyhub-rm566052 \
+#  --name mysql-root-password \
+#  --value "$MYSQL_ROOT_PASSWORD"
---
+++ create-containe-group.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+set -x
+
+export ACR_NAME=moneyhubrm566052
+export ACR_SERVER=${ACR_NAME}.azurecr.io
+export MYSQL_ROOT_PASSWORD=$(az keyvault secret show --vault-name kv-moneyhub-rm566052 --name mysql-root-password --query value --output tsv)
+export MYSQL_DATABASE=dv-dimdim
+export MYSQL_USER=$(az keyvault secret show --vault-name kv-moneyhub-rm566052 --name mysql-user --query value --output tsv)
+export MYSQL_PASSWORD=$(az keyvault secret show --vault-name kv-moneyhub-rm566052 --name mysql-password --query value --output tsv)
+export SPRING_DATASOURCE_URL=$(az keyvault secret show --vault-name kv-moneyhub-rm566052 --name spring-datasource-url --query value --output tsv)
+export SPRING_DATASOURCE_USER=$MYSQL_USER
+export SPRING_DARASOURCE_PASSWORD=$MYSQL_PASSWORD
+ADMIN_USERNAME=$(az acr credential show --name ${ACR_NAME} \
+                                        --resource-group rg-money-hub \
+                                        --query username --output tsv)
+ADMIN_PASSWORD=$(az acr credential show --name ${ACR_NAME} \
+                                        --resource-group rg-money-hub \
+                                 --query passwords[0].value --output tsv)
+az conatiner create \
+  --resource-group rg-money-hub \
+  --file aci-deploy.yaml \
+  --output table
